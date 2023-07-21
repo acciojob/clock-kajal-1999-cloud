e650d0a2057457ef9a4dec419e5ee17917d94056
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-21 18:25:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ function dateAndTime(){
      const year = currentTime.getFullYear();
      const month = currentTime.getMonth();
      const day = currentTime.getDay();
-     const hours = currentTime.getHours();
-     const minutes = currentTime.getMinutes();
-     const seconds = currentTime.getSeconds();
+ //     const hours = currentTime.getHours();
+ //     const minutes = currentTime.getMinutes();
+ //     const seconds = currentTime.getSeconds();
 
-	const amOrPm = hours >= 12 ? 'PM':'AM';
-	const adjustTime = hours%12 || 12;
-	 // "||" = logical operator if hours%12 gives 0 remainder then it will be consider 12 -->( || 12)
-  document.getElementById("timer").textContent = `${day}/${month}/${year}, ${singleValue(hours)}:${singleValue(minutes)}:${singleValue(seconds)} ${amOrPm}`;
+	// const amOrPm = hours >= 12 ? 'PM':'AM';
+	// const adjustTime = hours%12 || 12;
+	//  // "||" = logical operator if hours%12 gives 0 remainder then it will be consider 12 -->( || 12)
+ //  document.getElementById("timer").textContent = `${day}/${month}/${year}, ${singleValue(hours)}:${singleValue(minutes)}:${singleValue(seconds)} ${amOrPm}`;
+
+
+	// method-2
+const time = currentTime.toLocalTimeString();
+  document.getElementById("timer").textContent = `${day}/${month}/${year}, ${time}`;
+	
+
 }
 
 
@@ -21,3 +28,4 @@ function singleValue(value){
 };
 dateAndTime();
 setInterval(dateAndTime, 1000);
+
