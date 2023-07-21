ca638da2647578beb7ac76f6cb1a3bfd754de9b8
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-21 18:22:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+
+function dateAndTime(){
+	const currentTime = new Date();
+	
+     const year = currentTime.getFullYear();
+     const month = currentTime.getMonth();
+     const day = currentTime.getDay();
+     const hours = currentTime.getHours();
+     const minutes = currentTime.getMinutes();
+     const seconds = currentTime.getSeconds();
+
+	const amOrPm = hours >= 12 ? 'PM':'AM';
+	const adjustTime = hours%12 || 12;
+	 // "||" = logical operator if hours%12 gives 0 remainder then it will be consider 12 -->( || 12)
+  document.getElementById("timer").textContent = `${day}/${month}/${year}, ${singleValue(hours)}:${singleValue(minutes)}:${singleValue(seconds)} ${amOrPm}`;
+}
+
+
+function singleValue(value){
+	return value < 10 ? `0${value}`: value;
+};
+dateAndTime();
+setInterval(dateAndTime, 1000);
